61d1839b999ea3d3f678302c82934e068140cff8
liuhengloveyou <liuhengloveyou@gmail.com>
2015-01-20 18:51:10 +0800

    update

--- example/syncZmqServer.go
+++ example/syncZmqServer.go
@@ -10,12 +10,11 @@ func main() {
 	flag.Parse()
 
 	casper.BuildAppFromConfig("./casper.conf.example")
-	
+
 	com := casper.GetAppByName("syncService")
-//	com.SetHandler(handler)
+	com.SetHandler(handler)
 	com.Run()
 }
-
 
 func handler(msg *casper.Payload) (result interface{}, err error) {
 	return "this is syncService self", nil
